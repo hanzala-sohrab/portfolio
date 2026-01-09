94243e50ae93aae954c9f0570362b0945c6106ed
Hanzala Sohrab <sorb.hanzala@gmail.com>
2026-01-09 18:27:04 +0530

    fix: improve Navbar component for better time handling and code formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,32 @@
+import { useEffect, useState } from "react";
 import dayjs from "dayjs";
-import {navIcons, navLinks} from "#constants";
+import { navIcons, navLinks } from "#constants";
 import useWindowStore from "#store/window.js";
 
 const Navbar = () => {
-    const {openWindow} = useWindowStore();
+    const { openWindow } = useWindowStore();
+    const [time, setTime] = useState(dayjs());
+
+    useEffect(() => {
+        const id = setInterval(() => setTime(dayjs()), 1000);
+        return () => clearInterval(id);
+    }, []);
+
     return (
         <nav>
             <div>
                 <img src="/images/logo.svg" alt="logo" />
                 <p className="font-bold">Hanzala&apos;s portfolio</p>
                 <ul>
-                    {navLinks.map(({id, name, type}) => <li key={id} onClick={() => openWindow(type)} className="cursor-pointer">{name}</li>)}
+                    {navLinks.map(({ id, name, type }) => <li key={id} onClick={() => openWindow(type)} className="cursor-pointer">{name}</li>)}
                 </ul>
             </div>
             <div>
                 <ul>
-                    {navIcons.map(({id, img}) => <li key={id}><img src={img} alt={`icon-${id}`} className='icon-hover' /></li>)}
+                    {navIcons.map(({ id, img }) => <li key={id}><img src={img} alt={`icon-${id}`} className='icon-hover' /></li>)}
                 </ul>
 
-                <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+                <time>{time.format('ddd MMM D h:mm A')}</time>
             </div>
         </nav>
     )
